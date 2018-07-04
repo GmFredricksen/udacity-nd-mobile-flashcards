60f83e269231110da8eade46ac5e62722302d79b
GmFredricksen <xgiuliano.morox@gmail.com>
2018-07-05 01:41:08 +0200

    feat: add initial basic data seeder

---
+++ utils/seed-data.ts
@@ -0,0 +1,49 @@
+interface seedData {
+  key: string,
+  title: string,
+  questions: [
+    {
+      question: string,
+      answer: string,
+    }
+  ]
+}
+
+const seedData = [
+  {
+    key: 'React',
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  {
+    key: 'JavaScript',
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  },
+  {
+    key: 'Rust',
+    title: 'Rust',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+];
+
+export default seedData;
