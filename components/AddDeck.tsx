606ebc2be32d77039cece94b48559579c139e15a
Gm Fredricksen <xgiuliano.morox@gmail.com>
2018-07-07 20:52:15 +0200

    feat: implement AddDeck component view

---
+++ components/AddDeck.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  KeyboardAvoidingView,
+  View,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+
+interface AddDeck {
+  navigation: object,
+}
+
+class AddDeck extends Component {
+  state = {
+    titleText: '',
+  }
+
+  render() {
+    const { navigation } = this.props;
+
+
+    return (
+      <KeyboardAvoidingView style={styles.detailView} behavior="padding" enabled>
+        <TextInput
+          style={styles.titleInputField}
+          onBlur={Keyboard.dismiss}
+          onChangeText={(text) => this.setState({ text })}
+          placeholder="New Deck Title"
+          value={this.state.titleText}
+        />
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <View style={styles.buttonOutlined}>
+            <Text>Save Deck</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <View style={styles.buttonOutlined}>
+            <Text>Cancel</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    )
+  }
+};
+
+const styles = StyleSheet.create({
+  detailView: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20
+  },
+  titleInputField: {
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+  buttonOutlined: {
+    width: 200,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+  },
+});
+
+export default AddDeck;
