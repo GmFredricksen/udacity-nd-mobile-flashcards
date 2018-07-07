506d4f5b6ba13117d8516833154b2069637f8def
Gm Fredricksen <xgiuliano.morox@gmail.com>
2018-07-07 19:36:35 +0200

    feat: add buttons and basic styles in DeckDetails component view

--- components/DeckDetails.tsx
+++ components/DeckDetails.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import seedData from '../utils/seed-data';
 
 interface DeckDetails {
@@ -10,16 +11,53 @@ class DeckDetails extends Component {
   render() {
     const { navigation } = this.props;
     const { deckName } = navigation.state.params;
-    const deckObject = seedData.find((deck) => deck.title === deckName ) || null;
+    const deckObject = seedData.find((deck) => deck.title === deckName) || null;
 
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.detailView}>
         <Text>You have {deckObject && deckObject.questions.length} questions
-          for {deckName} subject. 
+          for {deckName}.
         </Text>
+        <TouchableOpacity>
+          <View style={styles.buttonItems}>
+            <Text>
+              <MaterialIcons name='add-to-photos' size={30}/>
+              Add Card
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.buttonOutlined}>
+            <Text>Start Quiz</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     )
   }
 };
+
+const styles = StyleSheet.create({
+  detailView: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20
+  },
+  buttonOutlined: {
+    width: 200,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+  },
+  buttonItems: {
+    width: 200,
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+  }
+});
 
 export default DeckDetails;
