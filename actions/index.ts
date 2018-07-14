8c5ea7aba7b0d37a411dafc0a30cfb3d0903c704
Gm Fredricksen <xgiuliano.morox@gmail.com>
2018-07-14 19:11:54 +0200

    feat: implement setDeck action

--- actions/index.ts
+++ actions/index.ts
@@ -1,15 +1,23 @@
 import { Action } from 'redux';
 
 export const SET_DECKS = 'SET_DECKS';
+export const SET_DECK = 'SET_DECK';
 
 export interface DecksInterface extends Action {
   type: string,
   decks?: object,
+  deck?: object,
 }
 
 export function setDecks(decks: object): DecksInterface {
   return {
     type: SET_DECKS,
     decks,
+  }
+}
+export function setDeck(deck: object): DecksInterface {
+  return {
+    type: SET_DECK,
+    deck,
   }
 }
