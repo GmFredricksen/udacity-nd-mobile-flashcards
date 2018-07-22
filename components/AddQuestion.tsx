9e846d698a325ff76fa0bb3398c03a363e5d4c25
GmFredricksen <xgiuliano.morox@gmail.com>
2018-07-22 22:18:18 +0200

    feat: implement picker in AddQuestion component
    * to store the correct answer for a Card

--- components/AddQuestion.tsx
+++ components/AddQuestion.tsx
@@ -8,6 +8,7 @@ import {
   View,
   TouchableOpacity,
   Keyboard,
+  Picker,
 } from 'react-native';
 
 import { addCardToDeck } from '../utils/api';
@@ -18,12 +19,14 @@ interface AddQuestionProps {
 interface AddQuestionState {
   question: string,
   answer: string,
+  result: boolean,
 }
 
 class AddQuestion extends Component<AddQuestionProps, AddQuestionState> {
   state = {
     question: '',
     answer: '',
+    result: true,
   }
 
   handleSaveQuestion() {
@@ -43,18 +46,36 @@ class AddQuestion extends Component<AddQuestionProps, AddQuestionState> {
       <KeyboardAvoidingView style={styles.detailView} behavior="padding" enabled>
         <TextInput
           style={styles.titleInputField}
+          multiline={true}
+          numberOfLines={3}
+          editable={true}
           onBlur={Keyboard.dismiss}
           onChangeText={(question) => this.setState({ question })}
           placeholder="Question"
           value={this.state.question}
         />
         <TextInput
-          style={styles.titleInputField}
+          style={styles.answerInputField}
+          multiline={true}
+          numberOfLines={3}
+          editable={true}
           onBlur={Keyboard.dismiss}
           onChangeText={(answer) => this.setState({ answer })}
           placeholder="Answer"
           value={this.state.answer}
         />
+        <View style={styles.pickerContainer}>
+          <Text>Result</Text>
+          <Picker
+            style={styles.picker}
+            itemStyle={styles.pickerItem}
+            selectedValue={this.state.result}
+            onValueChange={(itemValue, itemIndex) => this.setState({ result: itemValue })}
+          >
+            <Picker.Item label="Correct" value={true} />
+            <Picker.Item label="Incorrect" value={false} />
+          </Picker>
+        </View>
         <TouchableOpacity onPress={() => this.handleSaveQuestion()}>
           <View style={styles.buttonOutlined}>
             <Text>Save Card</Text>
@@ -78,10 +99,27 @@ const styles = StyleSheet.create({
     padding: 20
   },
   titleInputField: {
-    width: 200,
-    height: 40,
+    width: '90%',
+    height: '20%',
     borderWidth: 1,
     textAlign: 'center',
+  },
+  answerInputField: {
+    width: '90%',
+    height: '10%',
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+  pickerContainer: {
+    width: '90%',
+  },
+  picker: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 44,
+  },
+  pickerItem: {
+    height: 44,
   },
   buttonOutlined: {
     width: 200,
